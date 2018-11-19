b4f51d0f9cf78a98645766661ab8844ac983c00f
EtiChaudhary <cs17mtech11029@iith.ac.in>
2018-11-19 15:56:20 +0530

    Updated for Submission #2

--- svcomp19_config/pinaka-wrapper.sh
+++ svcomp19_config/pinaka-wrapper.sh
@@ -7,8 +7,8 @@ TOOL_NAME=Pinaka
 run()
 {
   programhash=$(sha256sum $BENCHMARK | awk '{print $1}')
-  #property=$(<$PROP_FILE)
-  ./$TOOL_BINARY $MODE $BIT_WIDTH --graphml-witness $WITNESS_FILE --hashval $programhash $BENCHMARK
+  # property=$(<$PROP_FILE)
+  ./$TOOL_BINARY $MODE $BIT_WIDTH --object-bits $OBJECT_BITS --graphml-witness $WITNESS_FILE --hashval $programhash --property-file $PROP_FILE $BENCHMARK
 }
 
 BIT_WIDTH="--64"
@@ -16,12 +16,14 @@ BENCHMARK=""
 PROP_FILE=""
 WITNESS_FILE=""
 MODE="--partial-incremental"
+OBJECT_BITS="11"
 
 while [ -n "$1" ] ; do
   case "$1" in
     --32|--64) BIT_WIDTH="$1" ; shift 1 ;;
     --propertyfile) PROP_FILE="$2" ; echo "PropertyFile Found" ; shift 2 ;;
     --graphml-witness) WITNESS_FILE="$2" ; shift 2 ;;
+    --version) echo "0.1"; exit 0 ;;
     *) BENCHMARK="$1" ; echo "BenchMark Found" ; shift 1 ;;
   esac
 done
@@ -33,6 +35,16 @@ fi
 
 if [ ! -s "$BENCHMARK" ] ; then
   echo "Empty benchmark or property file"
+  exit 1
+fi
+
+if [ -z "$PROP_FILE" ] ; then
+  echo "Missing property file"
+  exit 1
+fi
+
+if [ ! -s "$PROP_FILE" ] ; then
+  echo "Empty property file"
   exit 1
 fi
 # LOG_DIR="${TOOL_NAME}_Results/"
